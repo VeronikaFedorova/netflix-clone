2233ef80a7dc8b29cd32734343a0fa8901309f36
VeronikaFedorova <43063985+VeronikaFedorova@users.noreply.github.com>
2022-05-22 17:46:01 +0300

    Update HomeNav.js

--- src/components/HomeNav/HomeNav.js
+++ src/components/HomeNav/HomeNav.js
@@ -39,7 +39,7 @@ const HomeNav = ({updateData}) => {
         <img
           onClick={() => history.push("/profile")}
           className="nav__avatar"
-          src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+          src="./pngwing.com.png"
           alt="user logo"
         />
       </div>
