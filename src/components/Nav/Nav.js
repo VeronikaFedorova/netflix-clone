60555fdb2135e84aa854f4ad73993d0afe1c2eff
VeronikaFedorova <43063985+VeronikaFedorova@users.noreply.github.com>
2022-05-22 19:22:26 +0300

    Update Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -25,13 +25,13 @@ const Nav = () => {
         <img
           onClick={() => history.push("/")}
           className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="logo.png"
           alt="logo"
         />
         <img
           onClick={() => history.push("/profile")}
           className="nav__avatar"
-          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+          src="https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg"
           alt="user logo"
         />
       </div>
