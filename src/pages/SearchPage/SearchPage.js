ea5a4d48319565f48764f9f917fe84427696a357
VeronikaFedorova <43063985+VeronikaFedorova@users.noreply.github.com>
2022-05-23 18:33:24 +0300

    Update SearchPage.js

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -44,7 +44,7 @@ const SearchPage = () => {
           <img
             onClick={() => history.push("/")}
             className="nav__logo"
-            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            src="logo.png"
             alt="logo"
           />
           <input
@@ -56,7 +56,7 @@ const SearchPage = () => {
           <img
             onClick={() => history.push("/profile")}
             className="nav__avatar"
-            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/1/12/User_icon_2.svg"
             alt="user logo"
           />
         </div>
